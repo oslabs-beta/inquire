446bf5bca4e2582b0f24228a94536e2b26af95ea
CecilyJ <cecilyjansen@gmail.com>
2021-11-26 16:18:33 -0500

    preliminary algorithm to generate GQL schema to new file

--- testpkg/index.js
+++ testpkg/index.js
@@ -75,19 +75,20 @@ const parsedData = {
     ]
   }
 
-
+let data = [];
 function testpkg() {
   // fs.readFile('../data/testData/avscSample.avsc', 'utf-8', function(err, data) {
     try {
       // let parsedData = JSON.parse(data)
-      console.log(parsedData)
-      console.log("vvvvvvvvvvvvvvvvvvvvvvv")
-      console.log(parsedData.fields[2].type)
-      console.log("-----------------------")
-      console.log(parsedData.fields[2].type[1])
-      console.log("^^^^^^^^^^^^^^^^^^^^^^^")
-      console.log(parsedData.fields[2].type[1].items[1].fields)
-      console.log("------WARZONE-----")
+
+      // console.log(parsedData)
+      // console.log("vvvvvvvvvvvvvvvvvvvvvvv")
+      // console.log(parsedData.fields[2].type)
+      // console.log("-----------------------")
+      // console.log(parsedData.fields[2].type[1])
+      // console.log("^^^^^^^^^^^^^^^^^^^^^^^")
+      // console.log(parsedData.fields[2].type[1].items[1].fields)
+      // console.log("------WARZONE-----")
       let i = 0
       let res = []
       function backtrack(newObj) {
@@ -126,15 +127,62 @@ function testpkg() {
         }
       }
       backtrack(parsedData)
-      console.log(res)
-      return res
+      data = res;
+      return res;
 
     } catch(err) {
       console.log(err)
     }
   // })
 }
-//can use this function call for Quokka debugging
 testpkg();
+
+//algorithm to format data into GQL schema
+//iterate through data
+//array of arrays
+//Type _____ will be the first index in each array
+//any further indices are objects containing children of the main Type along with their respective data types
+//usually this is regular like int, float, etc. but can be Array. if inside the brackets it's a custom type, 
+//find out based on its contents which other parent Type will go inside those brackets in the generated schema
+//is it [currIdx where type array was found].type[1].items.name?
+const makeSchema = () => {
+  //iterate through data array variable
+  console.log("________MAKE SCHEMA________")
+  let result = `const { buildSchema } = require('graphql');\n\nmodule.exports = buildSchema(\`\n`
+
+  for (let i = data.length - 1; i >= 0; i--) {
+    let toAppend = '';
+    toAppend += `type ${data[i][0]} { \n`
+    
+    for (let j = 1; j < data[i].length; j++) {
+
+      const currProp = data[i][j];
+      const typeDef = String(currProp.type);
+      let currType = `${typeDef[0].toUpperCase().concat(typeDef.slice(1))}!`;
+
+      if (currType[0] === 'N') { //create an array with custom type inside - how to deal with arrays as a type if just regular data type wanted? like an array of Strings
+        currType = `[${currProp.type[1].items[1].name}]`;
+      } else if (currType[0] === '[') {
+        currType = `${currProp.type.name}!`
+      }
+      
+      toAppend += `   ${currProp.name}: ${currType} \n`
+    }
+    toAppend += '}\n'
+    result += toAppend;
+  }
+  result += '\`);'
+  console.log("han I miss you :(");
+  fs.writeFileSync('./newNewTest.js', result);
+}
+makeSchema();
+
+
+// let type = "person"
+  // let gqlSchema = `type ${type} {
+  //   name: ${typeof dataArr[0].name}
+  //   status: ${typeof dataArr[0].status}
+  //   xPos: ${typeof dataArr[0].location[0].x}
+  // }`
 
 // module.exports = {testpkg};
---
+++ testpkg/newNewTest.js
@@ -0,0 +1,22 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+type Trip { 
+   id: String! 
+   vehicleId: String! 
+   route: [Status] 
+}
+type Status { 
+   statusId: String! 
+   tripId: String! 
+   vehicleId: String! 
+   position: Position! 
+   batteryLevel: Int! 
+   distance: Int! 
+   timestamp: Int! 
+}
+type Position { 
+   lat: Float! 
+   lon: Float! 
+}
+`);
