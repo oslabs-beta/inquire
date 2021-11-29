8bce6a8ebc620cf106487f5008008dcf9ac01aed
CecilyJ <cecilyjansen@gmail.com>
2021-11-29 01:11:09 -0500

    set up for testing on separate branch to avoid merge conflicts

---
+++ data/testData/expAvVarSample.js
@@ -0,0 +1,31 @@
+const avro = require ('avsc');
+
+/* The purpose of this comment is to exist in a space where it should be 
+completely ignored. Rough! We want to grab the data after this.
+*/
+
+const schema = {
+  "fields": [
+    {
+      "name": "category",
+      "type": {
+        "name": "categoryType",
+        "symbols": [
+          "DOG",
+          "CAT"
+        ],
+        "type": "enum"
+      }
+    },
+    {
+      "name": "noise",
+      "type": "string"
+    }
+  ],
+  "name": "animals",
+  "type": "record"
+};
+
+const throwawayVar = "I should be ignored when extracting the above schema";
+
+module.exports =  avro.Type.forSchema(schema);
