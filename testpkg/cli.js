04536a6324edca665401cd20923f98e0848b4579
CecilyJ <cecilyjansen@gmail.com>
2021-12-10 19:58:39 -0500

    cli.js

---
+++ testpkg/cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const yargs = require('yargs');
+const path = require('path');
+const fs = require('fs');
+
+//goal is to get one string argument from this, send it to initTopiQL function in testpkg/startTopiQL.js
+const initTopiQL = require('./startTopiQL');
+const makeTopiQL = require('./makeTopiQL');
+
+//take one argument, relative path wherein to create topiQL folder
+const builder = command => {
+  command
+  .positional("absPath", {
+    describe: "absolute path to destination folder for topiQL"
+  })
+}
+
+const initHandler = ({absPath}) => {
+  fs.writeFileSync(path.resolve(__dirname, 'pathStore.json'), JSON.stringify(absPath));
+  console.log("destination", topiQLdest)
+  // initTopiQL(absPath); //will this work with Han's mode selection? Think so
+}
+
+const configHandler = () => {
+  const storedPath = path.resolve(__dirname, 'pathStore.json');
+  console.log(storedPath);
+  // if (fs.existsSync(storedPath)) {
+  //   const folderDest = JSON.parse(fs.readFileSync(storedPath));
+  //   makeTopiQL.toGraphQL(folderDest);
+  //   //read the file - if file exists, use the contents + topiQL/config.js
+  //   //hide file, potentially change its file properties
+  //   //makeTopiQL.toGraphQL(config);
+  // } else {
+  //   console.log("no config file found");
+  // }
+}
+
+yargs.command("init <absPath>", false, builder, initHandler).parse();
+yargs.command("config", false, builder, configHandler).parse();
