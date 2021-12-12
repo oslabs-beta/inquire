123773436c46d37d3beed8b533e44bd182e8760d
Anna Falvello <anna.falvello@gmail.com>
2021-12-12 17:51:43 -0500

    Added folder structure for client including redux file outlines. Updated webpack and server.js to serve necessary files for production mode. Added two style sheets for standard variables and text styles. Pulled index.html out to root directory

--- website/client/index.jsx
+++ website/client/index.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
-import App from './App.jsx';
-import ReactDOM from 'react-dom';
+/**
+ * ************************************
+ *
+ * @module  index.js
+ * @author
+ * @date
+ * @description entry point for application. Hangs React app off of #app in index.html
+ *
+ * ************************************
+ */
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ import React from 'react';
+ import { render } from 'react-dom';
+ import { Provider } from 'react-redux';
+ import App from './App.jsx';
+ import store from './store';
+ import styles from './stylesheets/styles.scss';
+ 
+ render(
+   // Wrap the App in the Provider Component and pass in the store
+   <Provider store={store}>
+     <App />
+   </Provider>,
+   document.getElementById('root')
+ );
+ 
